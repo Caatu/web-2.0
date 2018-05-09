9866a92a42c8bf1337b9f74fd86e2260cb2034f3
Albert <alberts258456@hotmail.com>
2018-05-09 16:07:35 -0300

    modificações nas urls e views para as medições serem retornadas em um json

--- web/urls.py
+++ web/urls.py
@@ -2,8 +2,15 @@ from django.urls import include, path, re_path
 import web.views as views
 urlpatterns = [
     path('', views.home, name='home'),
+
     path('unidade/<int:pk>/', views.local, name='locals'),
-    path('unidade/<int:unidade_pk>/local/<int:local_pk>/', views.sensors, name='sensors'),
-    path('unidade/<int:unidade_pk>/local/<int:local_pk>/sensor/<int:sensor_pk>/measurements',
-     views.measurements_sensor, name='measurements_sensor')
+
+    path('unidade/<int:unidade_pk>/local/<int:local_pk>',
+         views.sensors, name='sensors'),
+
+    path('unidade/<int:unidade_pk>/local/<int:local_pk>/sensor/<int:sensor_pk>/measurements/',
+         views.measurements_sensor, name='measurements_sensor'),
+
+    path('/ajax/sensor/<int:sensor_pk>',
+         views.measurements_sensor_ajax, name='measurements_sensor_ajax')
 ]
--- web/views.py
+++ web/views.py
@@ -1,11 +1,20 @@
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+import json
+
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponseForbidden
+from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
+from django.core.serializers.json import Serializer
+from django.http import HttpResponseForbidden, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
 import web.models as models
+
+
+class CustomSerializer(Serializer):
+    def get_dump_object(self, obj):
+        return self._current
+
 
 @login_required
 def home(request):
@@ -13,6 +22,7 @@ def home(request):
         'units': models.Unit.objects.filter(user=request.user)
     }
     return render(request, 'home.html', param)
+
 
 @login_required
 def local(request, pk):
@@ -25,8 +35,9 @@ def local(request, pk):
     }
     return render(request, 'locals.html', param)
 
+
 @login_required
-def sensors(request, unidade_pk ,local_pk):
+def sensors(request, unidade_pk, local_pk):
     unidade = get_object_or_404(models.Unit, pk=unidade_pk)
     local = get_object_or_404(models.Local, pk=local_pk)
     if unidade.user != request.user:
@@ -47,31 +58,60 @@ def sensors(request, unidade_pk ,local_pk):
 
 
 @login_required
+def measurements_sensor_ajax(request,sensor_pk):
+    """
+        Eu sei que esta uma porra.. mas funciona.
+    """
+    measurements = models.SensorMeasure.objects.filter(sensor=sensor_pk)
+    data = json.loads(
+        serializers.serialize(
+            'json',
+            measurements,
+            fields=('unit_of_measurement',
+                    'measurement_value', 'created_at')
+        )
+    )
+
+    return_data = []
+
+    for d in data:
+        return_data.append(d['fields'])
+
+    return JsonResponse(
+        return_data,
+        safe=False
+    )
+
+
+@login_required
 def measurements_sensor(request, unidade_pk, local_pk, sensor_pk):
     unidade = get_object_or_404(models.Unit, pk=unidade_pk)
     local = get_object_or_404(models.Local, pk=local_pk)
-    if(unidade.user != request.user):
+    if unidade.user != request.user:
         return HttpResponseForbidden()
-    measurements = models.SensorMeasure.objects.filter(sensor=sensor_pk)
+    collectors = models.Colector.objects.filter(local=local)
+    sensors_list = []
+    for index, collector in enumerate(collectors):
+        sensors = models.Sensor.objects.filter(colector=collector)
+        sensors_list.extend(sensors)
+        setattr(collector, 'quantity', len(collector.sensors.all()))
     param = {
-        'measurements': measurements,
         'unit': unidade,
         'local': local,
     }
     return render(request, 'measurements_sensor.html', param)
 
-@login_required
-def measurements_all(request, unidade_pk, local_pk):
-    unidade = get_object_or_404(models.Unit, pk=unidade_pk)
-    local = get_object_or_404(models.Local, pk=local_pk)
-    if(unidade.user != request.user):
-        return HttpResponseForbidden()
-    measurements = []
-    collectors = models.Colector.objects.filter(local=local)
-    for collector in collectors:
-        sensors = models.Sensor.objects.filter(colector=collector)
-        for sensor in sensors:
-            measurements.extend(models.SensorMeasure.objects.filter(sensor=sensor))
-    data = serializers.serializer('json', measurements)
-    return data
-    
+# @login_required
+# def measurements_all(request, unidade_pk, local_pk):
+#     unidade = get_object_or_404(models.Unit, pk=unidade_pk)
+#     local = get_object_or_404(models.Local, pk=local_pk)
+#     if(unidade.user != request.user):
+#         return HttpResponseForbidden()
+#     measurements = []
+#     collectors = models.Colector.objects.filter(local=local)
+#     for collector in collectors:
+#         sensors = models.Sensor.objects.filter(colector=collector)
+#         for sensor in sensors:
+#             measurements.extend(models.SensorMeasure.objects.filter(sensor=sensor))
+#     data = serializers.serializer('json', measurements)
+#     return data
